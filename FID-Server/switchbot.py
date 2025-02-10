5594b23242a8774152a1f5e7cd60613f84b10175
KIKU DEVELOP <109849033+KIKU-CORD@users.noreply.github.com>
2025-02-10 14:35:19 +0900

    Update switchbot.py

--- FID-Server/switchbot.py
+++ FID-Server/switchbot.py
@@ -7,8 +7,8 @@ import base64
 
 # APIの基本設定
 url = 'https://api.switch-bot.com'
-token = '6111becc84886a39aadfa6eae686d19f206bf5194b1ffaea6cd12171d44dfdfc30f186371294cd55054c9887145b0bd1'  # SwitchBotのトークン
-secret = 'dd8eb53d57bdf10876f483fa5be71581'  # SwitchBotのクライアントシークレット
+token = '???'  # SwitchBotのトークン
+secret = '???'  # SwitchBotのクライアントシークレット
 
 def control_device(deviceId, command):
     
